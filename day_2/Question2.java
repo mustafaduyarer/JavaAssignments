bb8037590bc08adc5bbfe6cbe00f7fbf024a5b52
mustafaduyarer <71519735+mustafaduyarer@users.noreply.github.com>
2020-09-18 22:15:58 +0200

    Add files via upload

---
+++ day_2/Question2.java
@@ -0,0 +1,17 @@
+package day_2;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		
+		String firstName = "Alan";
+	
+		System.out.println("My Name is " + firstName);
+		
+		String lastName = "Turing";
+		
+		System.out.println("My Last Name is " + lastName);
+
+	}
+
+}
---
+++ day_2/Question6.java
@@ -0,0 +1,25 @@
+package day_2;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("     ∆");
+		System.out.println("     /\\");
+		System.out.println("    /  \\");
+		System.out.println("   /    \\");
+		System.out.println("   |    |");
+		System.out.println("   |    |");
+		System.out.println("   |    |");
+		System.out.println("   |    |");
+		System.out.println("   |    |");
+		System.out.println("   |    |");
+		System.out.println("   |    |");
+		System.out.println("   |    |");
+		System.out.println("  --------");
+		
+		
+
+	}
+
+}
